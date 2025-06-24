046ff92430ee42343549f550069de3fa79228f9f
Dexter Aromin <dex.raromin@gmail.com>
2025-06-25 01:42:40 +0800

    new_project

---
+++ includes/header.php
@@ -0,0 +1,8 @@
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<link rel="stylesheet" type="image/x-icon" href="/assets/img/favicon_io/favicon.ico">
+	<link rel="stylesheet"  href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet"  href="assets/css/index.css">
+
+	<script src="assets/js/bootstrap.min.js"></script>
---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+    include 'includes/header.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Tunasan National High School - Main Page</title>
+</head>
+<body>
+
+<!--Navvbar-->
+    <nav class="navbar">
+      <div class="container">
+        <a class="navbar-brand" href="index.php">
+          <img src="assets/img/tunasan_logo.svg" alt="Tunasan Logo">
+        </a>
+      </div>
+    </nav>
+
+    <!--background -->
+
+
+</body>
+</html>
