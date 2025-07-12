202938bfc0ff01d7b0c6fd06876fca3207c71f86
Dexter Aromin <dex.raromin@gmail.com>
2025-07-12 17:40:26 +0800

    start of php script

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -21,10 +21,15 @@ $username = $decoded->username;
 
 require '../includes/header.php';
 require '../includes/admin_sidebar.php';
-
-
 ?>
 
+<?php
+    $junior_faculty = $conn->query("SELECT COUNT(*) as total FROM faculty_portal WHERE position = 'junior_high'")->fetch_assoc()["total"];
+    $senior_faculty = $conn->query("SELECT COUNT(*) as total FROM faculty_portal WHERE position = 'senior_high'")->fetch_assoc()["total"];
+    $junior_students = $conn->query("SELECT COUNT(*) as total FROM student_portal WHERE level = 'junior_high'")->fetch_assoc()["total"];
+    $senior_students = $conn->query("SELECT COUNT(*) as total FROM student_portal WHERE level = 'senior_high'")->fetch_assoc()["total"];
+
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -32,9 +37,66 @@ require '../includes/admin_sidebar.php';
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>TNHS Admin | Home</title>
-
 </head>
-<body>
+<style>
 
+
+
+</style>
+<body>
+    <div class="mamao">
+        <div class="row">
+            <div class="col-lg-3 col-sm-6">
+                <div class="card-box bg-blue">
+                    <div class="inner">
+                        <h3><?php echo $junior_students?></h3>
+                        <p>Junior High School Faculty</p>
+                    </div>
+                    <div class="icon">
+                        <i class="fa fa-chalkboard-user" aria-hidden="true"></i>
+                    </div>
+                    <a href="#" class="card-box-footer">View More <i class="fa fa-arrow-circle-right"></i></a>
+                </div>
+            </div>
+
+            <div class="col-lg-3 col-sm-6">
+                <div class="card-box bg-green">
+                    <div class="inner">
+                        <h3><?php echo $senior_students ?></h3>
+                        <p>Senior High School Faculty</p>
+                    </div>
+                    <div class="icon">
+                        <i class="fa fa-chalkboard-user" aria-hidden="true"></i>
+                    </div>
+                    <a href="#" class="card-box-footer">View More <i class="fa fa-arrow-circle-right"></i></a>
+                </div>
+            </div>
+            <div class="col-lg-3 col-sm-6">
+                <div class="card-box bg-orange">
+                    <div class="inner">
+                        <h3><?php echo $junior_faculty?></h3>
+                        <p>Junior High School Students</p>
+                    </div>
+                    <div class="icon">
+                        <i class="fa fa-users" aria-hidden="true"></i>
+                    </div>
+                    <a href="#" class="card-box-footer">View More <i class="fa fa-arrow-circle-right"></i></a>
+                </div>
+            </div>
+            <div class="col-lg-3 col-sm-6">
+                <div class="card-box bg-red">
+                    <div class="inner">
+                        <h3><?php echo $senior_faculty?></h3>
+                        <p>Senior High School Students</p>
+                    </div>
+                    <div class="icon">
+                        <i class="fa fa-users"></i>
+                    </div>
+                    <a href="#" class="card-box-footer">View More <i class="fa fa-arrow-circle-right"></i></a>
+                </div>
+            </div>
+
+        </div>
+    </div>
 </body>
 </html>
--- includes/admin_sidebar.php
+++ includes/admin_sidebar.php
@@ -70,6 +70,29 @@
         </div>
 
       </div>
+
+      <div class="topbar-controls">
+        <div class="dropdown">
+            <div class="user-menu" data-bs-toggle="dropdown" aria-expanded="false">
+                <div class="user-avatar">
+                    <?php
+                    $admin_initial = !empty($_SESSION['admin']) ? strtoupper(substr($_SESSION['username'], 0, 1)) : 'A';
+                    echo $admin_initial;
+                    ?>
+                </div>
+                <div class="user-info d-none d-xl-block">
+
+                </div>
+                <i class="bi bi-chevron-down d-none d-xl-block"></i>
+            </div>
+            <ul class="dropdown-menu dropdown-menu-end">
+                <li><a class="dropdown-item" href="account_management.php"><i class="bi bi-person"></i> Profile</a></li>
+                <li>
+                </li>
+                <li><a class="dropdown-item" href="../includes/logout.php"><i class="bi bi-box-arrow-right"></i>
+                        Logout</a></li>
+            </ul>
+        </div>
     </nav>
 
 
